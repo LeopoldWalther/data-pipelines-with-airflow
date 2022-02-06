9cb8c1b942d0b907d2234f292573f46086c4f854
LeopoldWalther <leopold.walther@gmail.com>
2022-02-06 19:03:04 +0100

    feat: complete implementation of pipeline

--- airflow/plugins/operators/load_dimension.py
+++ airflow/plugins/operators/load_dimension.py
@@ -11,12 +11,30 @@ class LoadDimensionOperator(BaseOperator):
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
+                 sql_stmt="",
+                 append=False,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
         # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.table=table
+        self.sql_stmt=sql_stmt
+        self.append=append
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info('Connect to Redshift')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        if self.append == False:
+            self.log.info(f"Delete table {self.table}")
+            redshift.run(f"DELETE FROM {self.table}")
+                       
+        self.log.info(f"Load data from S3 to Redshift into Dimension Table {self.table}")
+        redshift.run(f"INSERT INTO {self.table} {self.sql_stmt}")
+        self.log.info(f"Table {self.table} successfully loaded.")
+        
